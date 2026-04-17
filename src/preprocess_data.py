18fb9fa798e20f8753f782066320b29af2d7bd7a
AidanSm2103 <aidansm2103@gmail.com>
2026-04-17 10:17:25 +0200

    Updated preprocess data

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -2,22 +2,20 @@ import pandas as pd
 import joblib
 
 def preprocess_input(input_dict):
+
     feature_cols = joblib.load("artifacts/feature_columns.pkl")
     scaler = joblib.load("artifacts/scaler.pkl")
 
+    # Start with ALL features = 0
+    full_input = {col: 0 for col in feature_cols}
+
+    # Map user inputs into correct columns
+    for key, value in input_dict.items():
+        if key in full_input:
+            full_input[key] = value
+
     # Convert to DataFrame
-    df = pd.DataFrame([input_dict])
-
-    # One-hot encode
-    df = pd.get_dummies(df)
-
-    # Add missing columns
-    for col in feature_cols:
-        if col not in df:
-            df[col] = 0
-
-    # Remove extra columns
-    df = df[feature_cols]
+    df = pd.DataFrame([full_input])
 
     # Scale
     df_scaled = scaler.transform(df)
